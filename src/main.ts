570d23dd808f482fd8ae058f230b8c97013c83c2
CongGenZ <nvcong25042003@gmail.comm>
2024-10-02 01:38:32 +0700

    config connect FE and Be

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,12 @@ async function bootstrap() {
     }),
     
   );
+
+  // Bật CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Địa chỉ của frontend React
+    credentials: true, // Nếu cần gửi cookie
+  });
   app.useGlobalPipes(new ValidationPipe());
  
 await app.listen(3000);
